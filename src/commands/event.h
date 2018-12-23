11fa6805eae569120d99ab7b51964cc8acbaaa40
Paul Grosen <pcgrosen@gmail.com>
2018-12-22 23:50:29 -0800

    add similar filter to event system

--- src/commands/event.h
+++ src/commands/event.h
@@ -6,13 +6,21 @@
 class EventCommand : public ICommand
 {
   public:
-    inline static std::set<std::string> m_Hints = {};
+    inline static std::set<std::string>       m_Hints = {};
+    inline const static std::set<std::string> banned  = {
+		"load_game",
+		"new_game",
+		"continue_game",
+		"pause_game"
+	};
 
     virtual void Initialize() override
     {
         static hk::inject_jump<void, int64_t, char*, char, bool> add_event_hook(0x140089870);
         add_event_hook.inject([](int64_t _, char* eventName, char flags, bool unknown) {
-			m_Hints.insert(eventName);
+            if (banned.find(eventName) == banned.end()) {
+				m_Hints.insert(eventName);
+            }
             add_event_hook.call(_, eventName, flags, unknown);
 		});
     }
@@ -24,7 +32,7 @@ class EventCommand : public ICommand
 
     virtual bool Handler(const std::string& arguments) override
     {
-        if (arguments == "load_game" || arguments == "new_game" || arguments == "continue_game") {
+        if (banned.find(arguments) != banned.end()) {
             return false;
         }
 
