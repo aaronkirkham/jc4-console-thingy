9f7176c008f30fec4e992fe2efd7eabb225d7b13
aaronkirkham07@gmail.com <aaronkirkham07@gmail.com>
2020-02-25 21:12:43 +0000

    changes to teleport stuff to support updating existing locations (#11)

--- src/commands/teleport.h
+++ src/commands/teleport.h
@@ -1,67 +1,63 @@
 #pragma once
 
 #include "command.h"
-
-#include <fstream>
+#include "util.h"
 
 class TeleportCommand : public ICommand
 {
+  private:
+    util::KeyValueFile m_saveFile;
+
   public:
     virtual const char* GetCommand() override
     {
         return "tp";
     }
 
+    virtual void Init() override
+    {
+        m_saveFile.load("savedcoords.txt");
+    }
+
     virtual bool Handler(const std::string& arguments) override
     {
         if (arguments.find("save ") != std::string::npos) {
-            std::ofstream outfile("savedcoords.txt", std::ios::app);
-
             char name[128] = {0};
             if (sscanf_s(arguments.c_str(), "save %[^\n]s", &name, 128) == 1) {
-                outfile << name << ": ";
+                auto& transform = jc::CPlayerManager::instance().GetLocalPlayerCharacter()->m_transform;
+                m_saveFile.set(name, "%f %f %f", transform.m[3].x, transform.m[3].y, transform.m[3].z);
+                return true;
             }
-
-            auto& transform = jc::CPlayerManager::instance().GetLocalPlayerCharacter()->m_transform;
-            outfile << transform.m[3].x << " " << transform.m[3].y << " " << transform.m[3].z;
-            outfile << std::endl;
-
-            return true;
+        } else if (arguments.find("delete ") != std::string::npos) {
+            char name[128] = {0};
+            if (sscanf_s(arguments.c_str(), "delete %[^\n]s", &name, 128) == 1) {
+                m_saveFile.remove(name);
+                return true;
+            }
         } else {
-            static auto teleport = [](const std::string& str) {
-                CVector3f position{};
-                if (sscanf_s(str.c_str(), "%f %f %f", &position.x, &position.y, &position.z) == 3) {
-                    CMatrix4f world{};
-                    world.m[3].x = position.x;
-                    world.m[3].y = position.y;
-                    world.m[3].z = position.z;
+            static auto Teleport = [](const CVector3f& position) {
+                CMatrix4f world{};
+                world.m[3].x = position.x;
+                world.m[3].y = position.y;
+                world.m[3].z = position.z;
 
-                    jc::CGameWorld::instance().TeleportPlayer(&world, nullptr, false, false, 0.0f, 0.0f, 0, nullptr,
-                                                              false, nullptr);
-                    return true;
-                }
-
-                return false;
+                jc::CGameWorld::instance().TeleportPlayer(&world, nullptr, false, false, 0.0f, 0.0f, 0, nullptr, false,
+                                                          nullptr);
             };
 
             // try read coords straight from input (e.g. tp [x] [y] [z])
-            if (teleport(arguments)) {
+            CVector3f position;
+            if (sscanf_s(arguments.c_str(), "%f %f %f", &position.x, &position.y, &position.z) == 3) {
+                Teleport(position);
                 return true;
             }
-            // read saved position from coords file
+            // try read saved position from coords file
             else {
                 char name[128] = {0};
                 if (sscanf_s(arguments.c_str(), "%[^\n]s", &name, 128) == 1) {
-                    std::ifstream infile("savedcoords.txt");
-                    std::string   line;
-
-                    while (std::getline(infile, line)) {
-                        const auto pos = line.find(name);
-                        if (pos != std::string::npos) {
-                            // @NOTE: +2 for ": "
-                            const std::string coords = line.substr(pos + strlen(name) + 2, line.length());
-                            return teleport(coords);
-                        }
+                    if (m_saveFile.get(name, "%f %f %f", &position.x, &position.y, &position.z)) {
+                        Teleport(position);
+                        return true;
                     }
                 }
             }
@@ -74,11 +70,19 @@ class TeleportCommand : public ICommand
     {
         static std::array hints{
             "save",
+            "delete",
         };
 
         std::vector<std::string> result;
         std::copy_if(hints.begin(), hints.end(), std::back_inserter(result),
                      [&](const std::string& item) { return item.find(arguments) != std::string::npos; });
+
+        // copy saved coord names
+        for (const auto& entry : m_saveFile.Values) {
+            if (entry.first.find(arguments) != std::string::npos) {
+                result.push_back(entry.first);
+            }
+        }
 
         return result;
     }
--- src/util.h
+++ src/util.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <fstream>
+#include <map>
 #include <string>
 
 namespace util
 {
-template <typename... Args> std::string string_format(const std::string& format, Args... args)
+template <typename... Args> std::string string_format(const std::string &format, Args... args)
 {
     size_t                  size = snprintf(nullptr, 0, format.c_str(), args...) + 1;
     std::unique_ptr<char[]> buf(new char[size]);
@@ -25,7 +27,7 @@ static std::string GetClipboard(uint32_t max_length = 255)
     }
 
     std::string result;
-    if (char* text = static_cast<char*>(GlobalLock(handle))) {
+    if (char *text = static_cast<char *>(GlobalLock(handle))) {
         result = text;
 
         // strip characters
@@ -43,4 +45,68 @@ static std::string GetClipboard(uint32_t max_length = 255)
 
     return result;
 }
+
+class KeyValueFile
+{
+  public:
+    std::string                                  Filename;
+    std::unordered_map<std::string, std::string> Values;
+
+    KeyValueFile()  = default;
+    ~KeyValueFile() = default;
+
+    void load(const std::string &filename)
+    {
+        Filename = filename;
+
+        std::ifstream file(filename);
+        std::string   line;
+
+        while (file) {
+            std::getline(file, line);
+            if (line.length() > 0) {
+                const auto pos = line.find("\":");
+                if (pos != std::string::npos) {
+                    auto key   = line.substr(1, pos - 1);
+                    auto value = line.substr(pos + 2, line.length());
+
+                    Values[key] = value;
+                }
+            }
+        }
+    }
+
+    void save()
+    {
+        std::ofstream file(Filename);
+        for (const auto &entry : Values) {
+            file << "\"" << entry.first << "\":" << entry.second << std::endl;
+        }
+    }
+
+    template <typename... Args> void set(const std::string &key, const std::string &format, Args... args)
+    {
+        Values[key] = string_format(format, std::forward<Args>(args)...);
+        save();
+    }
+
+    int32_t get(const std::string &key, const char *format, ...)
+    {
+        if (Values.find(key) == Values.end()) {
+            return 0;
+        }
+
+        va_list args;
+        va_start(args, format);
+        auto result = vsscanf_s(Values[key].c_str(), format, args);
+        va_end(args);
+        return result;
+    }
+
+    void remove(const std::string &key)
+    {
+        Values.erase(key);
+        save();
+    }
+};
 }; // namespace util
